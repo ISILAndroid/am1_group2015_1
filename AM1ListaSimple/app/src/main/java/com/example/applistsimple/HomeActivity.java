35005326363d4305944aa62801da9b6de4a17157
Eduardo Medina Alfaro <emedinaa@gmail.com>
2015-05-22 17:07:19 -0500

    lista simple y lista completa

--- AM1ListaSimple/app/src/main/java/com/example/applistsimple/HomeActivity.java
+++ AM1ListaSimple/app/src/main/java/com/example/applistsimple/HomeActivity.java
@@ -9,72 +9,42 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
 public class HomeActivity extends Activity {
 
-	private String[] data = new String[]{ "Mercury", "Venus", "Earth", "Mars",  
-            "Jupiter", "Saturn", "Uranus", "Neptune"};
-	private ListView lstPlanets;
-	
+
+	private Button btnSimple, btnCompuesta;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
-        /**
-         * PASOS PARA TRABAJAR CON LISTAS Y ADAPTADORES
-         *  1. DATA , puede ser un ArrayList, List, XML
-         *  2. VIEW, normalmente es un listview, gridview o un spinner
-         *  3. ADAPTER , Arraydapter, BaseAdapter, Cursor Adapter
-         *  4. SETEAR ADAPTER AL VIEW
-         */
-		//listview
-		lstPlanets =(ListView)findViewById(R.id.lstPlanets);
-		//data
-		//adapter
-		ArrayAdapter<CharSequence> adapter =ArrayAdapter.createFromResource(this, 
-				R.array.arr_planets, android.R.layout.simple_list_item_1);
-		
-		ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, data);
-		lstPlanets.setAdapter(adapter2);
-		
-		//events
-		lstPlanets.setOnItemClickListener(new OnItemClickListener()
-        {
 
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position,
-					long id) {
-				// TODO Auto-generated method stub
-				String item= parent.getAdapter().getItem(position).toString();
-				Toast.makeText(HomeActivity.this, "CLICK "+item, Toast.LENGTH_SHORT).show();
-                gotoSimple(item);
-				
-			}
-		});
-		
-		lstPlanets.setOnItemLongClickListener(new OnItemLongClickListener() {
+        btnSimple= (Button)findViewById(R.id.btnSimple);
+        btnCompuesta= (Button)findViewById(R.id.btnCompuesta);
 
-			@Override
-			public boolean onItemLongClick(AdapterView<?> parent, View view,
-					int position, long id) {
-				// TODO Auto-generated method stub
-				
-				String item= parent.getAdapter().getItem(position).toString();
-				Toast.makeText(HomeActivity.this,"LONG "+ item, Toast.LENGTH_LONG).show();
-				return false;
-			}
-		});
+        //eventos
+
+        btnSimple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity.this,LSimpleActivity.class));
+            }
+        });
+
+        btnCompuesta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity.this,LCompuestaActivity.class));
+            }
+        });
+
 	}
 
-    private void gotoSimple(String item)
-    {
-        Intent intent= new Intent(this,SimpleActivity.class);
-        intent.putExtra("DATA",item);
-        startActivity(intent);
-    }
+
 
     @Override
 	public boolean onCreateOptionsMenu(Menu menu) {
